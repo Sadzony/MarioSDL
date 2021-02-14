89408810de68a4e64f9eb546067d90e91a0f0cac
Sadzony <szymon.sadunski@gmail.com>
2021-02-14 10:53:00 +0000

    Add character class

---
+++ MarioSDL/Character.cpp
@@ -0,0 +1,49 @@
+#include "Character.h"
+#include "Texture2D.h"
+Character::Character(SDL_Renderer* renderer, std::string imagePath, Vector2D start_position)
+{
+	m_renderer = renderer;
+	m_position = start_position;
+	m_texture = new Texture2D(m_renderer);
+	if (m_texture->LoadFromFile(imagePath) == false) {
+		std::cerr << "Image could not be loaded from " << imagePath << std::endl;
+	}
+}
+
+Character::~Character()
+{
+	m_renderer = nullptr;
+}
+
+void Character::Render()
+{
+	m_texture->Render(m_position, SDL_FLIP_NONE);
+}
+
+void Character::Update(float deltaTime, SDL_Event e)
+{
+	switch (e.type) 
+	{
+	case SDL_KEYDOWN:
+		switch (e.key.keysym.sym) 
+		{
+		case SDLK_LEFT:
+			m_position.x -= 1;
+			break;
+		case SDLK_RIGHT:
+			m_position.x += 1;
+			break;
+		}
+		break;
+	}
+}
+
+void Character::SetPosition(Vector2D new_position)
+{
+	m_position = new_position;
+}
+
+Vector2D Character::GetPosition()
+{
+	return m_position;
+}
---
+++ MarioSDL/Character.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include "Commons.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <string>
+class Texture2D;
+class Character
+{
+protected:
+	SDL_Renderer* m_renderer;
+	Vector2D m_position;
+	Texture2D* m_texture;
+public:
+	Character(SDL_Renderer* renderer, std::string imagePath, Vector2D start_position);
+	~Character();
+
+	virtual void Render();
+	virtual void Update(float deltaTime, SDL_Event e);
+	void SetPosition(Vector2D new_position);
+	Vector2D GetPosition();
+};
+
